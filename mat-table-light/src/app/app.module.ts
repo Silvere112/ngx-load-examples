4491e079519353d094b5f10c1f754be26e681e61
Silvère Marie <silvere@live.fr>
2022-03-12 21:55:21 +0100

    chore: add angular material to mat-table-light

--- mat-table-light/src/app/app.module.ts
+++ mat-table-light/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
