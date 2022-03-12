1cf009e5a38125ff7b76d95230c25cfc8f67d9d2
Silvère Marie <silvere@live.fr>
2022-03-12 22:02:22 +0100

    chore: add lod on mat-table-light

--- mat-table-light/src/app/app.module.ts
+++ mat-table-light/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoaderModule } from "ng-lod";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports: [BrowserModule, BrowserAnimationsModule, LoaderModule, MatButtonModule, MatTableModule],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
