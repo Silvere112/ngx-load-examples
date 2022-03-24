07e531db8ab8c399241f29bf6729d84104232955
Silvère Marie <silvere@live.fr>
2022-03-24 13:42:00 +0100

    feat: use ng-load 0.0.4

--- mat-table/src/app/app.module.ts
+++ mat-table/src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoaderModule } from "ng-lod";
 import { MatButtonModule } from "@angular/material/button";
 import { MatTableModule } from "@angular/material/table";
+import { NgxLoadModule } from "ngx-load";
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, LoaderModule, MatButtonModule, MatTableModule],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        NgxLoadModule,
+        MatButtonModule,
+        MatTableModule
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
